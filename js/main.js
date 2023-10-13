d35daf2ea174b5d2572e5ca6352f3a10767dc894
YvetteFB <fionabaraza@gmail.com>
2023-10-13 22:01:30 +0300

    suggested changes

--- js/main.js
+++ js/main.js
@@ -63,3 +63,31 @@
     
 })(jQuery);
 
+//Carousel
+$(function() {
+
+	if ( $('.owl-2').length > 0 ) {
+        $('.owl-2').owlCarousel({
+            center: false,
+            items: 1,
+            loop: true,
+            stagePadding: 0,
+            margin: 20,
+            smartSpeed: 1000,
+            autoplay: true,
+            pauseOnHover: false,
+            responsive:{
+                600:{
+                    margin: 20,
+                  items: 2
+                },
+                1000:{
+                    margin: 20,
+                    stagePadding: 0,
+                  items: 2
+                }
+            }
+        });            
+    }
+
+})
